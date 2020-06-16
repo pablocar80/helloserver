947dbb8dce965da862be0d8d261eb2c0df1ddeae
Pablo Carbonell <pablocar80@gmail.com>
2020-06-16 15:18:40 -0500

    added environment settings

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import * as dotenv from 'dotenv'
 import express from 'express'
 import bodyParser from 'body-parser';
 import { registerService } from './common'
 import { helloService } from './hello-service';
 
+dotenv.config()
+const port = +process.env.PORT || 3000
 const app = express()
-const port = 3000
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
